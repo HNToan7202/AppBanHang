1773de821cb0fc12caef45b8cb054fec5d40ac04
Nguyen Hoang Toan <nguyenhoangtoan@Nguyens-Laptop.local>
2023-01-13 10:55:21 +0700

    create view home

---
+++ app/src/main/java/vn/iotstar/appbanhang/MainActivity.java
@@ -0,0 +1,35 @@
+package vn.iotstar.appbanhang;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.ViewFlipper;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    ViewFlipper viewFlipper;
+    RecyclerView recyclerViewManHinhChinh;
+    NavigationView navigationView;
+    ListView listViewManHinhChinh;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        AnhXa();
+    }
+
+    private void AnhXa() {
+        toolbar = findViewById(R.id.toolbarmanhinhchinh);
+        viewFlipper = findViewById(R.id.flippermanhinhchinh);
+        recyclerViewManHinhChinh = findViewById(R.id.recycleview);
+        navigationView = findViewById(R.id.navigationview);
+        listViewManHinhChinh = findViewById(R.id.listviewmanhinhchinh);
+    }
+}
